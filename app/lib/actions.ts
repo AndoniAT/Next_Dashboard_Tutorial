779a3a1050939d0b81fb7514a6a40dfe8103db08
AndoniAT <andonialonsotort@gmail.com>
2024-03-18 23:34:08 +0100

    Edit an invoice

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -24,7 +24,8 @@ const FormSchema = z.object({
   });
    
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
- 
+const UpdateInvoice = FormSchema.omit({ id: true, date: true });
+
 export async function createInvoice(formData: FormData) {
     const { customerId, amount, status } = CreateInvoice.parse({
         customerId: formData.get('customerId'),
@@ -44,3 +45,23 @@ export async function createInvoice(formData: FormData) {
     revalidatePath('/dashboard/invoices'); // Once the database has been updated, the /dashboard/invoices path will be revalidated, and fresh data will be fetched from the server.
     redirect('/dashboard/invoices');
 }
+
+ 
+export async function updateInvoice(id: string, formData: FormData) {
+    const { customerId, amount, status } = UpdateInvoice.parse({
+      customerId: formData.get('customerId'),
+      amount: formData.get('amount'),
+      status: formData.get('status'),
+    });
+   
+    const amountInCents = amount * 100;
+   
+    await sql`
+      UPDATE invoices
+      SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+      WHERE id = ${id}
+    `;
+   
+    revalidatePath('/dashboard/invoices'); //  clear the client cache and make a new server request.
+    redirect('/dashboard/invoices'); //  redirect the user to the invoice's page.
+  }
